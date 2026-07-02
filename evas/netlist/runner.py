ee83fea97bfe2c73f5655dbfabf87f706dca314d
张托肯 <mac@GKVNWH2V60.local>
2026-07-02 11:20:21 +0800

    Fix runner import ordering

--- evas/netlist/runner.py
+++ evas/netlist/runner.py
@@ -16,10 +16,8 @@ import numpy as np
 from evas.compiler import ast_nodes as va_ast
 from evas.compiler.parser import (
     SpectreReservedIdentifierError,
-)
-from evas.compiler.parser import (
-    parse_all as parse_all_va,
     parse as parse_va,
+    parse_all as parse_all_va,
 )
 from evas.compiler.preprocessor import preprocess
 from evas.simulator.backend import compile_module
